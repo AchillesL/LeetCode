bae67d32d3a5f87c79d0f23dc1e553d42df4ec2c
liangzengguo <liangzengguo@163.com>
2020-09-13 17:35:10 +0800

    1480

--- LeetCode1480.java
+++ LeetCode1480.java
@@ -5,6 +5,7 @@ public class LeetCode1480 {
 
     }
 
+    /*用sum记录前n个数的和，再与第n+1个数相加，得到结果的第n+1个数。*/
     public int[] runningSum(int[] nums) {
         int[] result = new int[nums.length];
         int sum = 0;
